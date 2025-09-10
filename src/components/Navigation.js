7af8656c7074f99b69a99b530919d8d494d4bbe5
Sameer <sampom7@gmail.com>
2025-09-10 21:40:23 +0530

    nav-query update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
     <nav
-      className="lg:w-[40%] sm:w-[80%] w-[90%] flex justify-around align-middle lg:mt-16
+      className="hidden lg:w-[40%] sm:w-[80%] w-[90%] flex justify-around align-middle lg:mt-16
     sm:mt-24 mt-20
      border border-solid border-cyan sm:rounded-lg rounded-md"
     >
